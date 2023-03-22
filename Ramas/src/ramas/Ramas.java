e310da373116e6abe8b60a1954c201dbc94bb173
Luis Diaz <shichisekai@gmail.com>
2023-03-22 14:45:57 -0300

    Primer commit de rama 1

--- Ramas/src/ramas/Ramas.java
+++ Ramas/src/ramas/Ramas.java
@@ -15,7 +15,7 @@ public class Ramas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Aca estoy en la primera rama");
     }
     
     
